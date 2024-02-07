42edbf41f97aca1d36a596b38e5d8af72f6d679e
Maksim Levin <delinrus@gmail.com>
2024-02-07 17:24:16 +0300

    Prompt template

--- main/main.py
+++ main/main.py
@@ -1,10 +1,19 @@
 import os
 
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAI
 
 if __name__ == '__main__':
-    llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
+    llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), model="gpt-3.5-turbo-instruct", temperature=0)
 
-    response = llm.invoke("What is Neo4j?")
+    template = PromptTemplate(template="""
+    You are a cockney fruit and vegetable seller.
+    Your role is to assist your customer with their fruit and vegetable needs.
+    Respond using cockney rhyming slang.
+
+    Tell me about the following fruit: {fruit}
+    """, input_variables=["fruit"])
+
+    response = llm.invoke(template.format(fruit="apple"))
 
     print(response)
