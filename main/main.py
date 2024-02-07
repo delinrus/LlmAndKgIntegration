f3888843292e6523b056100787e7f1cc8178ccc6
Maksim Levin <delinrus@gmail.com>
2024-02-07 17:00:19 +0300

    Simple llm invocation

--- main/main.py
+++ main/main.py
@@ -1,3 +1,10 @@
+import os
+
+from langchain_openai import OpenAI
 
 if __name__ == '__main__':
-    print("Hello world!")
+    llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
+
+    response = llm.invoke("What is Neo4j?")
+
+    print(response)
