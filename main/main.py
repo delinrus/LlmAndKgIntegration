743b1a149cf88a8bc95ac059e1c334ed61ff1d30
Maksim Levin <delinrus@gmail.com>
2024-02-07 17:34:34 +0300

    Added LLMChain to work with prompt templates

--- main/main.py
+++ main/main.py
@@ -1,19 +1,25 @@
 import os
 
+from langchain.chains import LLMChain
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAI
 
 if __name__ == '__main__':
     llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), model="gpt-3.5-turbo-instruct", temperature=0)
 
-    template = PromptTemplate(template="""
+    template = PromptTemplate.from_template("""
     You are a cockney fruit and vegetable seller.
     Your role is to assist your customer with their fruit and vegetable needs.
     Respond using cockney rhyming slang.
-
+    
     Tell me about the following fruit: {fruit}
-    """, input_variables=["fruit"])
+    """)
 
-    response = llm.invoke(template.format(fruit="apple"))
+    llm_chain = LLMChain(
+        llm=llm,
+        prompt=template
+    )
+
+    response = llm_chain.invoke({"fruit": "apple"})
 
     print(response)
