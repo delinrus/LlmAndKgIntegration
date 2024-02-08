277813df6e205e48416bd45c191bda2b847cdaab
Maksim Levin <delinrus@gmail.com>
2024-02-08 11:55:49 +0300

    Giving context

--- main/main.py
+++ main/main.py
@@ -11,13 +11,28 @@ if __name__ == '__main__':
         template="""You are a surfer dude, having a conversation about the surf conditions on the beach.
     Respond using surfer slang.
 
+    Context: {context}
     Question: {question}
     """,
-        input_variables=["question"],
+        input_variables=["context", "question"],
     )
 
     chat_chain = LLMChain(llm=chat_llm, prompt=prompt)
 
-    response = chat_chain.invoke({"question": "What is the weather like?"})
+    current_weather = """
+        {
+            "surf": [
+                {"beach": "Fistral", "conditions": "6ft waves and offshore winds"},
+                {"beach": "Polzeath", "conditions": "Flat and calm"},
+                {"beach": "Watergate Bay", "conditions": "3ft waves and onshore winds"}
+            ]
+        }"""
+
+    response = chat_chain.invoke(
+        {
+            "context": current_weather,
+            "question": "What is the weather like on Watergate Bay?",
+        }
+    )
 
     print(response)
