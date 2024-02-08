6a6fc17ce834205c9bafa5eaf1fb1da7c9d8da8a
Maksim Levin <delinrus@gmail.com>
2024-02-08 12:41:05 +0300

    Running agent with one tool

--- main/main.py
+++ main/main.py
@@ -1,8 +1,11 @@
 import os
 
+from langchain import hub
+from langchain.agents import create_react_agent, AgentExecutor
 from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
 from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import Tool
 from langchain_openai import ChatOpenAI
 
 if __name__ == '__main__':
@@ -10,33 +13,36 @@ if __name__ == '__main__':
 
     prompt = PromptTemplate(
         template="""
-    You are a surfer dude, having a conversation about the surf conditions on the beach.
-    Respond using surfer slang.
+        You are a movie expert. You find movies from a genre or plot.
 
-    Chat History: {chat_history}
-    Context: {context}
-    Question: {question}
-    """,
-        input_variables=["chat_history", "context", "question"],
+        ChatHistory:{chat_history}
+        Question:{input}
+        """,
+        input_variables=["chat_history", "input"],
     )
 
-    memory = ConversationBufferMemory(
-        memory_key="chat_history", input_key="question", return_messages=True
-    )
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-    chat_chain = LLMChain(llm=chat_llm, prompt=prompt, memory=memory, verbose=True)
+    chat_chain = LLMChain(llm=llm, prompt=prompt, memory=memory)
 
-    current_weather = """
-        {
-            "surf": [
-                {"beach": "Fistral", "conditions": "6ft waves and offshore winds"},
-                {"beach": "Bells", "conditions": "Flat and calm"},
-                {"beach": "Watergate Bay", "conditions": "3ft waves and onshore winds"}
-            ]
-        }"""
+    tools = [
+        Tool.from_function(
+            name="Movie Chat",
+            description="For when you need to chat about movies. The question will be a string. Return a string.",
+            func=chat_chain.run,
+            return_direct=True,
+        )
+    ]
+
+    agent_prompt = hub.pull("hwchase17/react-chat")
+    agent = create_react_agent(llm, tools, agent_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, memory=memory)
 
     while True:
-        question = input("> ")
-        response = chat_chain.invoke({"context": current_weather, "question": question})
+        q = input("> ")
+        response = agent_executor.invoke({"input": q})
+        print(response["output"])
 
-        print(response["text"])
+
+# delinrus@gmail.com
+# neo4j+s://09b90eb07e931168e98bd970c1385828.neo4jsandbox.com:7687
