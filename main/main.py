503c216f74bc554d5f56110c727b46bd31b99402
Maksim Levin <delinrus@gmail.com>
2024-02-08 16:26:52 +0300

    Similarity search in using vector index

--- main/main.py
+++ main/main.py
@@ -1,21 +1,28 @@
-if __name__ == '__main__':
-    from langchain_community.graphs import Neo4jGraph
+import os
 
-    graph = Neo4jGraph(
+from langchain_community.vectorstores.neo4j_vector import Neo4jVector
+from langchain_openai import OpenAIEmbeddings
+
+if __name__ == '__main__':
+    embedding_provider = OpenAIEmbeddings(
+        openai_api_key=os.getenv("OPENAI_API_KEY")
+    )
+    movie_plot_vector = Neo4jVector.from_existing_index(
+        embedding_provider,
         url="bolt://54.86.80.87:7687",
         username="neo4j",
-        password="petition-week-rear"
+        password="petition-week-rear",
+        index_name="moviePlots",
+        embedding_node_property="embedding",
+        text_node_property="plot",
     )
 
-    result = graph.query("""
-    MATCH (m:Movie{title: 'Toy Story'}) 
-    RETURN m.title, m.plot, m.poster
-    """)
+    # query = "Give western movie where group of heroes protect innocent people"
+    query = "A movie where aliens land and attack earth."
 
-    print("Result")
-    print(result)
-    print("Graph schema")
-    print(graph.schema)
+    result = movie_plot_vector.similarity_search(query, k=1)
+    for doc in result:
+        print(doc.metadata["title"], "-", doc.page_content)
 
 # delinrus@gmail.com
 # neo4j+s://09b90eb07e931168e98bd970c1385828.neo4jsandbox.com:7687
