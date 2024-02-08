13d9527409a1057daa8600b05a389ab2cc8aa986
Maksim Levin <delinrus@gmail.com>
2024-02-08 12:48:47 +0300

    Chaining few tools

--- main/main.py
+++ main/main.py
@@ -4,12 +4,13 @@ from langchain import hub
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
+from langchain_community.tools.youtube.search import YouTubeSearchTool
 from langchain_core.prompts import PromptTemplate
 from langchain_core.tools import Tool
 from langchain_openai import ChatOpenAI
 
 if __name__ == '__main__':
-    chat_llm = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
+    llm = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
 
     prompt = PromptTemplate(
         template="""
@@ -25,18 +26,33 @@ if __name__ == '__main__':
 
     chat_chain = LLMChain(llm=llm, prompt=prompt, memory=memory)
 
+    youtube = YouTubeSearchTool()
+
     tools = [
         Tool.from_function(
             name="Movie Chat",
             description="For when you need to chat about movies. The question will be a string. Return a string.",
             func=chat_chain.run,
             return_direct=True,
-        )
+        ),
+        Tool.from_function(
+            name="Movie Trailer Search",
+            description="Use when needing to find a movie trailer. The question will include the word 'trailer'. Return a link to a YouTube video.",
+            func=youtube.run,
+            return_direct=True,
+        ),
     ]
 
     agent_prompt = hub.pull("hwchase17/react-chat")
     agent = create_react_agent(llm, tools, agent_prompt)
-    agent_executor = AgentExecutor(agent=agent, tools=tools, memory=memory)
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=tools,
+        memory=memory,
+        max_interations=3,
+        verbose=True,
+        handle_parse_errors=True,
+    )
 
     while True:
         q = input("> ")
